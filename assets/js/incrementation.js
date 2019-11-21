83d9449681dd1bb150f1c761620bd16c039eba24
SquirrelJo <caroline.dirat@gmail.com>
2019-11-21 09:08:23 +0100

    commentaires CSS

--- assets/js/incrementation.js
+++ assets/js/incrementation.js
@@ -4,12 +4,12 @@ const minus = document.querySelector(".fa-minus"); // bouton "moins" devant "Ser
 // contenu textuel de l'élément HTML où est indiqué le nombre de parts de la recette
 const number = document.getElementById("number"); 
 
-// quand on clique sur l'icône "+", le nombre de personne est incrémenté de 1
+// quand on clique sur l'icône "+", le nombre de parts est incrémenté de 1
 plus.addEventListener("click", function(){
 	number.textContent = Number(number.textContent) + 1;
 });
 
-// quand on clique sur l'icône "+", le nombre de personne est décrémenté de 1
+// quand on clique sur l'icône "+", le nombre de parts est décrémenté de 1
 minus.addEventListener("click", function(){
 	if(Number(number.textContent)>0) {
 		number.textContent = Number(number.textContent) - 1;
